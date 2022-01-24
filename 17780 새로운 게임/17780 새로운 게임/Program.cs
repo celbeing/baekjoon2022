736afccf422fa7dfcd1c8f82b63452fd2665823d
celbeing <kimsd1983@naver.com>
2022-01-24 22:59:54 +0900

    Update Program.cs

--- 17780 새로운 게임/17780 새로운 게임/Program.cs
+++ 17780 새로운 게임/17780 새로운 게임/Program.cs
@@ -1,8 +1,22 @@
 ﻿using System;
 class Program
 {
+    static int n, k;
     static void Main()
     {
-
+        var nk = Console.ReadLine().Split();
+        n = int.Parse(nk[0]);
+        k = int.Parse(nk[1]);
+        int[,] board = new int[n, n];
+        int[,,]
+        for(int i = 0; i < n; i++)
+        {
+            var row = Console.ReadLine().Split();
+            for(int j = 0; j < n; j++)
+            {
+                board[i, j] = int.Parse(row[j]);
+            }
+        }
+        
     }
 }
