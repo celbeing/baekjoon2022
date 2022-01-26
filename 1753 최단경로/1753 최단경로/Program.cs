053519af7571c0e2e382d341cea7478246bd4856
celbeing <kimsd1983@naver.com>
2022-01-27 00:14:07 +0900

    22.01.26

--- 1753 최단경로/1753 최단경로/Program.cs
+++ 1753 최단경로/1753 최단경로/Program.cs
@@ -3,54 +3,25 @@ using System.Text;
 using System.Collections.Generic;
 class Program
 {
-    static int V, E, K, u, v, w;
+    static int N, x, y;
     static StringBuilder result = new StringBuilder();
-    static Queue<int> bfs = new Queue<int>();
+    static List<(int, int)> coordinate = new List<(int, int)>();
     static void Main()
     {
-        var VE = Console.ReadLine().Split();
-        V = int.Parse(VE[0]);
-        E = int.Parse(VE[1]);
-        K = int.Parse(Console.ReadLine()) - 1;
-        int[] distance = new int[V];
-        int[,] graph = new int[V, V];
-        for (int i = 0; i < E; i++)
+        N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
         {
-            var uvw = Console.ReadLine().Split();
-            u = int.Parse(uvw[0]) - 1;
-            v = int.Parse(uvw[1]) - 1;
-            w = int.Parse(uvw[2]);
-            graph[u, v] = w;
+            var xy = Console.ReadLine().Split();
+            coordinate.Add((int.Parse(xy[0]), int.Parse(xy[1])));
         }
-        bfs.Enqueue(K);
-        while (bfs.Count > 0)
+        coordinate.Sort();
+        for(int i = 0; i < N; i++)
         {
-            for (int i = 0; i < V; i++)
-            {
-                if (graph[bfs.Peek(), i] != 0)
-                {
-                    if (distance[i] != 0)
-                    {
-                        if (distance[bfs.Peek()] + graph[bfs.Peek(), i] < distance[i])
-                        {
-                            distance[i] = distance[bfs.Peek()] + graph[bfs.Peek(), i];
-                            bfs.Enqueue(i);
-                        }
-                    }
-                    else
-                    {
-                        distance[i] = distance[bfs.Peek()] + graph[bfs.Peek(), i];
-                        bfs.Enqueue(i);
-                    }
-                }
-            }
-            bfs.Dequeue();
+            result.AppendLine(coordinate[i].ToString());
         }
-        for (int i = 0; i < V; i++)
-        {
-            if (i == K) result.AppendLine(0.ToString());
-            else result.AppendLine(distance[i] == 0 ? "INF" : distance[i].ToString());
-        }
+        result.Replace("(", "");
+        result.Replace(")", "");
+        result.Replace(",", "");
         Console.WriteLine(result);
     }
 }
