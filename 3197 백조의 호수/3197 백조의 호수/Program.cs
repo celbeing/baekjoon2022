120a0b4f9b656e01f37428e446f7c6af0eae627b
celbeing <kimsd1983@naver.com>
2022-02-06 18:06:04 +0900

    22.02.06
    맞았습니다!

--- 3197 백조의 호수/3197 백조의 호수/Program.cs
+++ 3197 백조의 호수/3197 백조의 호수/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-public class loc
+public class Location
 {
     public int x;
     public int y;
-    public loc(int a, int b)
+    public Location(int a, int b)
     {
         x = a;
         y = b;
@@ -16,10 +16,10 @@ class Program
     static int[,] lake = new int[1500, 1500];
     static bool[,] wcheck = new bool[1500, 1500];
     static bool[,] scheck = new bool[1500, 1500];
-    static Queue<loc> water = new Queue<loc>();
-    static Queue<loc> Nwater = new Queue<loc>();
-    static Queue<loc> swan = new Queue<loc>();
-    static Queue<loc> Nswan = new Queue<loc>();
+    static Queue<Location> water = new Queue<Location>();
+    static Queue<Location> Nwater = new Queue<Location>();
+    static Queue<Location> swan = new Queue<Location>();
+    static Queue<Location> Nswan = new Queue<Location>();
     static void Main()
     {
         var rc = Console.ReadLine().Split();
@@ -31,7 +31,7 @@ class Program
             {
                 if (row[j] == '.') lake[i, j] = 0;      // 0 : water
                 else if (row[j] == 'X') lake[i, j] = 1; // 1 : glacier
-                else { lake[i, j] = -1; Console.WriteLine("Swan here"); }                   // -1: swan
+                else lake[i, j] = -1;                   // -1: swan
             }
         }
         for (int i = 0; i < R; i++)
@@ -40,12 +40,12 @@ class Program
             {
                 if (lake[i, j] < 1)
                 {
-                    water.Enqueue(new loc(i, j));
+                    water.Enqueue(new Location(i, j));
                     wcheck[i, j] = true;
                 }
                 if (lake[i, j] == -1)
                 {
-                    swan.Enqueue(new loc(i, j));
+                    swan.Enqueue(new Location(i, j));
                     scheck[i, j] = true;
                 }
             }
@@ -54,58 +54,54 @@ class Program
         swan.Dequeue();
         while (true)
         {
-            Console.WriteLine($"days gone... today is {day}");
             while (Nwater.Count > 0)
             {
                 r = Nwater.Peek().x; c = Nwater.Peek().y;
                 if (lake[r, c] == 1) lake[r, c] = 0;
-                water.Enqueue(new loc(r, c));
+                water.Enqueue(new Location(r, c));
                 Nwater.Dequeue();
             }                               // Nwater 빙하 녹이고
                                             // water  으로 큐 옮기기
             while (water.Count > 0)
             {
                 r = water.Peek().x; c = water.Peek().y;
-                Console.WriteLine($"check water {r},{c}");
                 if (r > 0 && !wcheck[r - 1, c])
                 {
-                    if (lake[r - 1, c] == 1) Nwater.Enqueue(new loc(r - 1, c));
-                    else water.Enqueue(new loc(r - 1, c));
+                    if (lake[r - 1, c] == 1) Nwater.Enqueue(new Location(r - 1, c));
+                    else water.Enqueue(new Location(r - 1, c));
                     wcheck[r - 1, c] = true;
                 }
                 if (c > 0 && !wcheck[r, c - 1])
                 {
-                    if (lake[r, c - 1] == 1) Nwater.Enqueue(new loc(r, c - 1));
-                    else water.Enqueue(new loc(r, c - 1));
+                    if (lake[r, c - 1] == 1) Nwater.Enqueue(new Location(r, c - 1));
+                    else water.Enqueue(new Location(r, c - 1));
                     wcheck[r, c - 1] = true;
                 }
                 if (r < R - 1 && !wcheck[r + 1, c])
                 {
-                    if (lake[r + 1, c] == 1) Nwater.Enqueue(new loc(r + 1, c));
-                    else water.Enqueue(new loc(r + 1, c));
+                    if (lake[r + 1, c] == 1) Nwater.Enqueue(new Location(r + 1, c));
+                    else water.Enqueue(new Location(r + 1, c));
                     wcheck[r + 1, c] = true;
                 }
                 if (c < C - 1 && !wcheck[r, c + 1])
                 {
-                    if (lake[r, c + 1] == 1) Nwater.Enqueue(new loc(r, c + 1));
-                    else water.Enqueue(new loc(r, c + 1));
+                    if (lake[r, c + 1] == 1) Nwater.Enqueue(new Location(r, c + 1));
+                    else water.Enqueue(new Location(r, c + 1));
                     wcheck[r, c + 1] = true;
                 }                               // water  좌표 상하좌우 확인하고
                                                 // 빙하 있으면 Nwater에 Enqueue,
                                                 // 빙하 없으면  water에 Enqueue
                 water.Dequeue();
             }
-            Console.WriteLine($"{day} Water check done");
             while (Nswan.Count > 0)
             {
                 r = Nswan.Peek().x; c = Nswan.Peek().y;
-                swan.Enqueue(new loc(r, c));
+                swan.Enqueue(new Location(r, c));
                 Nswan.Dequeue();
             }                               // Nswan에서 swan으로 큐 옮기기
             while (swan.Count > 0)
             {
                 r = swan.Peek().x; c = swan.Peek().y;
-                Console.WriteLine($"check swan {r},{c}");
                 if (r > 0 && !scheck[r - 1, c])
                 {
                     if(lake[r - 1, c] == -1)
@@ -113,8 +109,8 @@ class Program
                         Console.WriteLine(day);
                         return;
                     }
-                    else if (lake[r - 1, c] == 1) Nswan.Enqueue(new loc(r - 1, c));
-                    else swan.Enqueue(new loc(r - 1, c));
+                    else if (lake[r - 1, c] == 1) Nswan.Enqueue(new Location(r - 1, c));
+                    else swan.Enqueue(new Location(r - 1, c));
                     scheck[r - 1, c] = true;
                 }
                 if (c > 0 && !scheck[r, c - 1])
@@ -124,8 +120,8 @@ class Program
                         Console.WriteLine(day);
                         return;
                     }
-                    else if (lake[r, c - 1] == 1) Nswan.Enqueue(new loc(r, c - 1));
-                    else swan.Enqueue(new loc(r, c - 1));
+                    else if (lake[r, c - 1] == 1) Nswan.Enqueue(new Location(r, c - 1));
+                    else swan.Enqueue(new Location(r, c - 1));
                     scheck[r, c - 1] = true;
                 }
                 if (r < R - 1 && !scheck[r + 1, c])
@@ -135,8 +131,8 @@ class Program
                         Console.WriteLine(day);
                         return;
                     }
-                    else if (lake[r + 1, c] == 1) Nswan.Enqueue(new loc(r + 1, c));
-                    else swan.Enqueue(new loc(r + 1, c));
+                    else if (lake[r + 1, c] == 1) Nswan.Enqueue(new Location(r + 1, c));
+                    else swan.Enqueue(new Location(r + 1, c));
                     scheck[r + 1, c] = true;
                 }
                 if (c < C - 1 && !scheck[r, c + 1])
@@ -146,13 +142,12 @@ class Program
                         Console.WriteLine(day);
                         return;
                     }
-                    else if (lake[r, c + 1] == 1) Nswan.Enqueue(new loc(r, c + 1));
-                    else swan.Enqueue(new loc(r, c + 1));
+                    else if (lake[r, c + 1] == 1) Nswan.Enqueue(new Location(r, c + 1));
+                    else swan.Enqueue(new Location(r, c + 1));
                     scheck[r, c + 1] = true;
                 }
                 swan.Dequeue();
             }
-            Console.WriteLine($"{day} Swan check done");
             day++;
         }
     }
