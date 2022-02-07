06714336df5929cc49c553b1bf3e67c628d4a985
celbeing <kimsd1983@naver.com>
2022-02-07 14:20:04 +0900

    22.02.07

---
+++ 11401 이항 계수 3/11401 이항 계수 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+class Program
+{
+    static int N, K;
+    static BigInteger bc = new BigInteger();
+    static void Main()
+    {
+        var nk = Console.ReadLine().Split();
+        N = int.Parse(nk[0]);
+        K = int.Parse(nk[1]);
+        bc = 1;
+        if (N < K * 2) K = N - K;
+        for(int i = N - K; i <= N; i++)
+        {
+            bc *= i;
+        }
+        bc /= N - K;
+        for(int i = 1; i <= K; i++)
+        {
+            bc /= i;
+        }
+        Console.WriteLine(bc);
+    }
+}
---
+++ 18291 비요뜨의 징검다리 건너기/18291 비요뜨의 징검다리 건너기/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18291_비요뜨의_징검다리_건너기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
