9b0552a96ae8d4b8de536a1b98b092b129580741
celbeing <kimsd1983@naver.com>
2022-02-07 16:11:30 +0900

    22.02.07
    분할 정복 알고리즘

---
+++ 11051 이항 계수 2/11051 이항 계수 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static int N, K;
+    static int div = 10007;
+    static void Main()
+    {
+        var nk = Console.ReadLine().Split();
+        N = int.Parse(nk[0]); K = int.Parse(nk[1]);
+        Console.WriteLine(Fac(N) * Pow(Fac(N - K) * Fac(K) % div, div - 2) % div);
+    }
+    static int Fac(int f)
+    {
+        if (f < 2) return 1;
+        else return f * Fac(f - 1) % div;
+    }
+    static int Pow(int b, int e)
+    {
+        int res = 1;
+        while (e > 0)
+        {
+            if (e % 2 == 1)
+            {
+                res *= b;
+                res %= div;
+                e--;
+            }
+            b *= b;
+            b %= div;
+            e >>= 1;
+        }
+        return res;
+    }
+}
---
+++ 1629 곱셈/1629 곱셈/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static long A;
+    static int B, C;
+    static long res = 1;
+
+    static void Main()
+    {
+        var ABC = Console.ReadLine().Split();
+        A = long.Parse(ABC[0]);
+        B = int.Parse(ABC[1]);
+        C = int.Parse(ABC[2]);
+        while (B > 0)
+        {
+            if (B % 2 == 1)
+            {
+                res *= A;
+                res %= C;
+                B--;
+            }
+            A *= A;
+            A %= C;
+            B >>= 1;
+        }
+        Console.WriteLine(res);
+    }
+}
