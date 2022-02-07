00f2bde46c11df4ee6d8c516fb091371ade0bd6c
celbeing <kimsd1983@naver.com>
2022-02-07 15:28:58 +0900

    22.02.07
    분할 정복 알고리즘, 고속 거듭제곱

--- 18291 비요뜨의 징검다리 건너기/18291 비요뜨의 징검다리 건너기/Program.cs
+++ 18291 비요뜨의 징검다리 건너기/18291 비요뜨의 징검다리 건너기/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _18291_비요뜨의_징검다리_건너기
+class Program
 {
-    class Program
+    static int T, N;
+    static ulong div = 1000000007;
+    static void Main()
     {
-        static void Main(string[] args)
+        T = int.Parse(Console.ReadLine());
+        for(int i = 0; i < T; i++)
         {
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine(Power(2, N - 2) % div);
         }
+    }
+    static ulong Power(ulong a, int n)
+    {
+        ulong res = 1;
+        while(n > 0)
+        {
+            if(n % 2 == 1)
+            {
+                res *= a;
+                res %= div;
+                n--;
+            }
+            a *= a;
+            a %= div;
+            n >>= 1;
+        }
+        return res;
     }
 }
