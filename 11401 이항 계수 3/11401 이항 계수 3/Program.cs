ea61de7165fbf6fefd449524fa1f34106ea52b6a
celbeing <kimsd1983@naver.com>
2022-02-07 17:13:06 +0900

    22.02.07
    메모리 너무 많이 먹음. 분할 정복 알고리즘

--- 11401 이항 계수 3/11401 이항 계수 3/Program.cs
+++ 11401 이항 계수 3/11401 이항 계수 3/Program.cs
@@ -1,25 +1,37 @@
 ﻿using System;
-using System.Numerics;
 class Program
 {
     static int N, K;
-    static BigInteger bc = new BigInteger();
+    static int div = 1000000007;
+    static long[] fac = new long[4000001];
+    static long[] inv = new long[4000001];
     static void Main()
     {
+        fac[1] = 1;
+        for(int i = 2; i <= 4000000; i++)
+            fac[i] = fac[i - 1] * i % div;
+        inv[4000000] = (int)Pow(fac[4000000], div - 2);
+        for (int i = 3999999; i >= 0; i--)
+            inv[i] = inv[i + 1] * (i + 1) % div;
         var nk = Console.ReadLine().Split();
-        N = int.Parse(nk[0]);
-        K = int.Parse(nk[1]);
-        bc = 1;
-        if (N < K * 2) K = N - K;
-        for(int i = N - K; i <= N; i++)
+        N = int.Parse(nk[0]); K = int.Parse(nk[1]);
+        Console.WriteLine((fac[N] * inv[N - K] % div) * inv[K] % div);
+    }
+    static long Pow(long b, int e)
+    {
+        long res = 1;
+        while (e > 0)
         {
-            bc *= i;
+            if (e % 2 == 1)
+            {
+                res *= b;
+                res %= div;
+                e--;
+            }
+            b *= b;
+            b %= div;
+            e >>= 1;
         }
-        bc /= N - K;
-        for(int i = 1; i <= K; i++)
-        {
-            bc /= i;
-        }
-        Console.WriteLine(bc);
+        return res;
     }
 }
