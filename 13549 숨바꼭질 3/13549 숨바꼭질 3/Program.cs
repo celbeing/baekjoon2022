8bdaa5cb72920cc25bc26eeaf6c9b3c80f80e872
celbeing <kimsd1983@naver.com>
2022-02-09 20:26:04 +0900

    22.02.09
    뭔가 이상함

--- 13549 숨바꼭질 3/13549 숨바꼭질 3/Program.cs
+++ 13549 숨바꼭질 3/13549 숨바꼭질 3/Program.cs
@@ -3,7 +3,8 @@ using System.Linq;
 using System.Collections.Generic;
 class Program
 {
-    static int N, K, s;
+    static int N, K, S;
+    static bool find = false;
     static int[] time = Enumerable.Repeat<int>(100000, 200001).ToArray<int>();
     static bool[] check = new bool[200001];
     static Queue<int> BFS = new Queue<int>();
@@ -16,32 +17,43 @@ class Program
         check[N] = true;
         while (BFS.Count > 0)
         {
-            s = BFS.Peek();
-            if (s == K) break;
-            Search(s);
+            S = BFS.Peek();
+            if (find) break;
+            Search(S);
             BFS.Dequeue();
         }
         Console.WriteLine(time[K]);
     }
     static void Search(int s)
     {
-        int[] ds = {s, 1, -1};
-        for(int i = 0; i < 3; i++)
+        find = s == K ? true : find;
+        if (find) return;
+        Teleport(s);
+        Walk(s);
+    }
+    static void Teleport(int s)
+    {
+        find = s == K ? true : find;
+        check[s] = true;
+        if (s * 2 < K * 2 && !check[s * 2])
         {
-            if (s + ds[i] < 0 || s + ds[i] > K * 2) continue;
-            if (!check[s + ds[i]])
+            time[s * 2] = time[s];
+            Search(s * 2);
+        }
+    }
+    static void Walk(int s)
+    {
+        find = s == K ? true : find;
+        check[s] = true;
+        int[] ds = { -1, 1 };
+        for(int i = 0; i < 2; i++)
+        {
+            if (s + ds[i] < 0 || s + ds[i] > 100000) continue;
+            if(!check[s + ds[i]])
             {
-                check[s + ds[i]] = true;
-                if (i == 0)
-                {
-                    time[s + ds[i]] = time[s];
-                    Search(s + ds[i]);
-                }
-                else
-                {
-                    time[s + ds[i]] = time[s] + 1;
-                    BFS.Enqueue(s + ds[i]);
-                }
+                time[s + ds[i]] = Math.Min(time[s] + 1, time[s + ds[i]]);
+                Teleport(s + ds[i]);
+                BFS.Enqueue(s + ds[i]);
             }
         }
     }
