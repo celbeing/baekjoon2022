81c065e8771fdbaf0fd5715f1c5b7b49cde9ff72
celbeing <kimsd1983@naver.com>
2022-02-10 17:06:01 +0900

    22.02.10

--- 13549 숨바꼭질 3/13549 숨바꼭질 3/Program.cs
+++ 13549 숨바꼭질 3/13549 숨바꼭질 3/Program.cs
@@ -4,57 +4,50 @@ using System.Collections.Generic;
 class Program
 {
     static int N, K, S;
-    static bool find = false;
-    static int[] time = Enumerable.Repeat<int>(100000, 200001).ToArray<int>();
-    static bool[] check = new bool[200001];
+    static int[] time = Enumerable.Repeat<int>(-1, 200001).ToArray<int>();
     static Queue<int> BFS = new Queue<int>();
+
     static void Main()
     {
         var nk = Console.ReadLine().Split();
         N = int.Parse(nk[0]); K = int.Parse(nk[1]);
+        if(N >= K)  // 거꾸로 가는 케이스 제거
+        {
+            Console.WriteLine(N == K ? 0 : N - K);
+            return;
+        }
         BFS.Enqueue(N);
         time[N] = 0;
-        check[N] = true;
         while (BFS.Count > 0)
         {
-            S = BFS.Peek();
-            if (find) break;
-            Search(S);
-            BFS.Dequeue();
-        }
-        Console.WriteLine(time[K]);
-    }
-    static void Search(int s)
-    {
-        find = s == K ? true : find;
-        if (find) return;
-        Teleport(s);
-        Walk(s);
-    }
-    static void Teleport(int s)
-    {
-        find = s == K ? true : find;
-        check[s] = true;
-        if (s * 2 < K * 2 && !check[s * 2])
-        {
-            time[s * 2] = time[s];
-            Search(s * 2);
-        }
-    }
-    static void Walk(int s)
-    {
-        find = s == K ? true : find;
-        check[s] = true;
-        int[] ds = { -1, 1 };
-        for(int i = 0; i < 2; i++)
-        {
-            if (s + ds[i] < 0 || s + ds[i] > 100000) continue;
-            if(!check[s + ds[i]])
+            S = BFS.Dequeue();
+            //Console.WriteLine(S);
+            if (time[K] != -1 && time[K] < time[S]) break;
+            int[] d = { 0, -1, 1 };
+            for (int i = 0; i < 3; i++)
             {
-                time[s + ds[i]] = Math.Min(time[s] + 1, time[s + ds[i]]);
-                Teleport(s + ds[i]);
-                BFS.Enqueue(s + ds[i]);
+                if (S + d[i] < 0 || S + d[i] > K + 2) continue; // 신경 안 써도 되는 케이스 건너뛰기
+                if (i == 0 && S != 0) // 순간이동 하는 경우
+                {
+                    for(int j = 2; S * j < K + 2; j *= 2)
+                    {
+                        if(time[S * j] == -1 || time[S] < time[S * j])
+                        {
+                            time[S * j] = time[S];
+                            BFS.Enqueue(S * j);
+                        }
+                    }
+                }
+                else        // 걸어가는 경우
+                {
+                    if (time[S + d[i]] == -1 || time[S + d[i]] > time[S] + 1)
+                    {       // 처음 왔거나 최단거리인 경우
+                        time[S + d[i]] = time[S] + 1;
+                        BFS.Enqueue(S + d[i]);
+                    }
+                }
             }
         }
+        Console.WriteLine(time[K]);
     }
 }
