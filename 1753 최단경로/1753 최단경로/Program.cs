71a1e0fbacbd4dea88b99dacd619d73e1c3d0554
celbeing <kimsd1983@naver.com>
2022-02-11 17:20:11 +0900

    22.02.11

---
+++ 1753 최단경로/1753 최단경로/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static int V, E;
+    static Queue<int> BFS = new Queue<int>();
+    static void Main()
+    {
+        var ve = Console.ReadLine().Split();
+        V = int.Parse(ve[0]); E = int.Parse(ve[1]);
+        BFS.Enqueue(int.Parse(Console.ReadLine()));
+        int[,] node = new int[V, V];
+    }
+}
