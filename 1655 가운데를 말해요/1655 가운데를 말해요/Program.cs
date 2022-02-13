5669887a6483b6ac4f8f8d1a82ee96e1c5f26730
celbeing <kimsd1983@naver.com>
2022-02-13 12:41:41 +0900

    22.02.13
    최소, 최대 힙

--- 1655 가운데를 말해요/1655 가운데를 말해요/Program.cs
+++ 1655 가운데를 말해요/1655 가운데를 말해요/Program.cs
@@ -1,106 +1,110 @@
 ﻿﻿using System;
+using System.IO;
+using System.Linq;
 class Program
 {
-    static int N, Index_max, Index_min, change, tmp;
-    static int[] Max = new int[50001];  // 가장 큰 값이 1번으로 오는 최대 힙
-    static int[] Min = new int[50001];  // 가장 작은 값이 1번으로 오는 최소 힙
+    static int N, index_max, index_min, tmp;
+    static int[] Max = Enumerable.Repeat(int.MinValue, 100001).ToArray<int>();
+    static int[] Min = Enumerable.Repeat(int.MaxValue, 100001).ToArray<int>();
+    static BufferedStream bs = new BufferedStream(Console.OpenStandardOutput());
+    static StreamWriter Speak = new StreamWriter(bs);
     static void Main()
     {
-        Index_max = 2; Index_min = 2;
+        index_max = 1; index_min = 1;
         N = int.Parse(Console.ReadLine());
-        Max[1] = int.Parse(Console.ReadLine());
-        Console.WriteLine(Max[1]);
-        if (N > 1)
+        for(int i = 0; i < N; i++)
         {
-            Min[1] = int.Parse(Console.ReadLine());
-            if(Max[1] > Min[1])
-            {
-                change = Max[1];
-                Max[1] = Min[1];
-                Min[1] = change;
-            }
-            Console.WriteLine(Max[1]);
+            if (i % 2 == 0) MaxInsert(int.Parse(Console.ReadLine()));
+            else MinInsert(int.Parse(Console.ReadLine()));
+            Speak.WriteLine(Max[1]);
         }
-        for (int i = 2; i < N; i++)
-        {
-            if (i % 2 == 0)
-            {
-                Max_Heap(int.Parse(Console.ReadLine()));
-            }
-            else
-            {
-                Min_Heap(int.Parse(Console.ReadLine()));
-            }
-            Console.WriteLine($"{Index_max} {Max[5]} {Max[4]} {Max[3]} {Max[2]} {Max[1]} {Min[1]} {Min[2]} {Min[3]} {Min[4]} {Min[5]} {Index_min}");
-        }
+        Speak.Close();
     }
-    static void Top_Change()            // 힙 1번 값을 비교하고 최대힙>최소힙 이면 서로 바꿔서 정렬 다시하기
+
+    static void MaxInsert(int k)
     {
-        if (Index_min == 1 || Index_max == 1) return;
-        if (Max[1] > Min[1])
+        if(Min[1] < k)
         {
-            tmp = Max[1];
-            Index_max--;        // 여기 정렬 안함
-            Max_Heap(Min[1]);
-            Index_min--;
-            Min_Heap(tmp);
-        }
-    }
-    static void Max_Heap(int k)         // 최대 힙에 k를 추가하고 정렬
-    {
-        if (k > Min[1])
-        {
-            Max[Index_max] = Min[1];
-            Index_min--;
-            Push(Min, Index_min);
-            Min_Heap(k);
+            Max[index_max] = Min[1];
+            MaxSortUp();
+            Min[1] = k;
+            MinSortDown();
         }
         else
         {
-            Max[Index_max] = k;
+            Max[index_max] = k;
+            MaxSortUp();
         }
-        for (int i = Index_max; i > 1; i /= 2)
+        index_max++;
+    }
+    static void MaxSortUp()
+    {
+        for(int i = index_max; i > 1; i /= 2)
         {
-            if (Max[i / 2] < Max[i])
+            if (Max[i] > Max[i / 2])
             {
-                change = Max[i];
+                tmp = Max[i];
                 Max[i] = Max[i / 2];
-                Max[i / 2] = change;
+                Max[i / 2] = tmp;
             }
             else break;
         }
-        Index_max++;
     }
-    static void Min_Heap(int k)         // 최소 힙에 k를 추가하고 정렬
+    static void MaxSortDown()
     {
-        if(k < Max[1])
+        for(int i = 1; i <= index_max;)
         {
-            Min[Index_min] = Max[1];
-            Index_max--;
-            Push(Max, Index_max);
-            Max_Heap(k);
+            if (Max[i] < Math.Max(Max[i * 2], Max[i * 2 + 1]))
+            {
+                i = Max[i * 2] > Max[i * 2 + 1] ? i * 2 : i * 2 + 1;
+                tmp = Max[i / 2];
+                Max[i / 2] = Max[i];
+                Max[i] = tmp;
+            }
+            else break;
+        }
+    }
+    static void MinInsert(int k)
+    {
+        if (Max[1] > k)
+        {
+            Min[index_min] = Max[1];
+            MinSortUp();
+            Max[1] = k;
+            MaxSortDown();
         }
         else
         {
-            Min[Index_min] = k;
+            Min[index_min] = k;
+            MinSortUp();
         }
-        for (int i = Index_min; i > 1; i /= 2)
+        index_min++;
+    }
+    static void MinSortUp()
+    {
+        for (int i = index_min; i > 1; i /= 2)
         {
-            if (Min[i / 2] > Min[i])
+            if (Min[i] < Min[i / 2])
             {
-                change = Min[i];
+                tmp = Min[i];
                 Min[i] = Min[i / 2];
-                Min[i / 2] = change;
+                Min[i / 2] = tmp;
             }
             else break;
         }
-        Index_min++;
     }
-    static void Push(int[] Heap, int Index)
+    static void MinSortDown()
     {
-        for(int i = 1; i < Index; i++)
+        for(int i = 1; i <= index_min;)
         {
-            Heap[i] = Heap[i + 1];
+            if (Min[i] > Math.Min(Min[i * 2], Min[i * 2 + 1]))
+            {
+                i = Min[i * 2] < Min[i * 2 + 1] ? i * 2 : i * 2 + 1;
+                tmp = Min[i / 2];
+                Min[i / 2] = Min[i];
+                Min[i] = tmp;
+            }
+            else break;
         }
     }
 }
