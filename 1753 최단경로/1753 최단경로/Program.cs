72ce431a2043207c1c76c734cc7f7e74a016ff76
celbeing <kimsd1983@naver.com>
2022-02-13 15:00:32 +0900

    22.02.12

--- 1753 최단경로/1753 최단경로/Program.cs
+++ 1753 최단경로/1753 최단경로/Program.cs
@@ -2,101 +2,24 @@
 using System.Linq;
 using System.Text;
 using System.Collections.Generic;
-public class GraphNode<T>
+
+public class Graph
 {
-    private List<GraphNode<T>> _linked;
-    private List<int> _weight;
-    public T data { get; set; }
-    public GraphNode() { }
-    public GraphNode(T value)
+    private int from, to;
+    public Graph(int n1, int n2)
     {
-        this.data = value;
-    }
-    public List<GraphNode<T>> Linked
-    {
-        get
-        {
-            _linked = _linked ?? new List<GraphNode<T>>();
-            return _linked;
-        }
-    }
-    public List<int> Weight
-    {
-        get
-        {
-            _weight = _weight ?? new List<int>();
-            return _weight;
-        }
-    }
-}
-public class Graph<T>
-{
-    private List<GraphNode<T>> _nodeList;
-    public Graph()
-    {
-        _nodeList = new List<GraphNode<T>>();
-    }
-    public GraphNode<T> AddNode(T data)
-    {
-        GraphNode<T> n = new GraphNode<T>(data);
-        _nodeList.Add(n);
-        return n;
-    }
-    public GraphNode<T> AddNode(GraphNode<T> node)
-    {
-        _nodeList.Add(node);
-        return node;
-    }
-    public void AddEdge(GraphNode<T> from, GraphNode<T> to, int weight = 0, bool oneway = true)
-    {
-        from.Linked.Add(to);
-        from.Weight.Add(weight);
-        if (!oneway)
-        {
-            to.Linked.Add(from);
-            to.Weight.Add(weight);
-        }
-    }
-    internal void DebugPrintLinks()
-    {
-        foreach(GraphNode<T> graphNode in _nodeList)
-        {
-            foreach(var n in graphNode.Linked)
-            {
-                string s = graphNode.data + " - " + n.data;
-                Console.WriteLine(s);
-            }
-        }
+        from = n1;
+        to = n2;
     }
 }
 class Program
 {
-    static void Main()
-    {
-        Graph<int> g = new Graph<int>();
-        var n1 = g.AddNode(10);
-        n1 = g.AddNode(60);
-        var n2 = g.AddNode(20);
-        var n3 = g.AddNode(30);
-        var n4 = g.AddNode(40);
-        var n5 = g.AddNode(50);
-
-        g.AddEdge(n1, n3, 4);
-        g.AddEdge(n2, n4, 1);
-        g.AddEdge(n3, n4, 2);
-        g.AddEdge(n3, n5, 5);
-
-        g.DebugPrintLinks();
-    }
-}
-
-/*
-class Program
-{
-    static int V, E, ex,ey, S;
+    static int V, E, W, S;
     static int[] dist = Enumerable.Repeat<int>(int.MaxValue, 20000).ToArray<int>();
     static int[,] edge = new int[20000, 20000];
     static bool[] check = new bool[20000];
+    static List<Graph> Node = new List<Graph>();
+    static List<int> Weight = new List<int>();
     static Queue<int> BFS = new Queue<int>();
     static StringBuilder res = new StringBuilder();
     static void Main()
@@ -108,9 +31,12 @@ class Program
         for (int i = 0; i < E; i++)
         {
             var e = Console.ReadLine().Split();
-            ex = int.Parse(e[0]) - 1; ey = int.Parse(e[1]) - 1;
-            if (edge[ex, ey] == 0) edge[ex, ey] = int.Parse(e[2]);
-            else edge[ex, ey] = Math.Min(edge[ex, ey], int.Parse(e[2]));
+            Graph edge = new Graph(int.Parse(e[0]) - 1, int.Parse(e[1]) - 1);
+            W = int.Parse(e[2]);
+            if (Node.Contains(edge))
+            {
+                List[Node.FindIndex(edge)] = Math.Min(List[Node.FindIndex(edge)], W);
+            }
         }
         while(BFS.Count > 0)
         {
@@ -138,4 +64,3 @@ class Program
         //Console.Write(res);
     }
 }
-*/
