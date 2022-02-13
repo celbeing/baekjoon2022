8226818af427defa50f6cb90b3b6bcf1dfbe3053
celbeing <kimsd1983@naver.com>
2022-02-13 12:41:54 +0900

    22.02.13
    0-1 BFS

---
+++ 6087 레이저 통신/6087 레이저 통신/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static int W, H;
+    static void Main()
+    {
+        var wh = Console.ReadLine().Split();
+        W = int.Parse(wh[0]); H = int.Parse(wh[1]);
+        char[,] map = new char[H, W];
+        for(int i = 0; i < H; i++)
+        {
+            var row = Console.ReadLine();
+            for (int j = 0; j < W; j++) map[i, j] = row[j];
+        }
+    }
+}
