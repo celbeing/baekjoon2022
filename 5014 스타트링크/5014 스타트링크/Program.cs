38f6e3201f21189a0ecd8fe20e876044fa7017a8
celbeing <kimsd1983@naver.com>
2022-02-15 13:29:47 +0900

    22.02.15

--- 5014 스타트링크/5014 스타트링크/Program.cs
+++ 5014 스타트링크/5014 스타트링크/Program.cs
@@ -10,24 +10,24 @@ class Program
         var fsgud = Console.ReadLine().Split();
         F = int.Parse(fsgud[0]); S = int.Parse(fsgud[1]); G = int.Parse(fsgud[2]);
         U = int.Parse(fsgud[3]); D = int.Parse(fsgud[4]);
-        int[] count = Enumerable.Repeat(int.MaxValue, F).ToArray<int>();
-        F--; S--; G--;
+        int[] count = Enumerable.Repeat(-1, F).ToArray<int>();
+        S--; G--;
         BFS.Enqueue(S);
         count[S] = 0;
         while(BFS.Count > 0)
         {
             now = BFS.Dequeue();
-            if(now + U <= F && count[now + U] == int.MaxValue)
+            if(now + U < F && count[now + U] == -1)
             {
                 count[now + U] = count[now] + 1;
                 BFS.Enqueue(now + U);
             }
-            if (now - D > 0 && count[now - D] == int.MaxValue)
+            if (now - D > -1 && count[now - D] == -1)
             {
                 count[now - D] = count[now] + 1;
                 BFS.Enqueue(now - D);
             }
         }
-        Console.WriteLine(count[G] == int.MaxValue ? "use the stairs" : count[G]);
+        Console.WriteLine(count[G] == -1 ? "use the stairs" : count[G]);
     }
 }
