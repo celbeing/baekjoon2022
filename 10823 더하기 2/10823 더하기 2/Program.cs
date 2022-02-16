632ab1a014ced9dba6972c80908424cd90efb5b7
celbeing <kimsd1983@naver.com>
2022-02-16 22:45:28 +0900

    22.02.16

---
+++ 10823 더하기 2/10823 더하기 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+class Program
+{
+    static int res = 0;
+    static string input;
+    static StringBuilder S = new StringBuilder();
+    static void Main()
+    {
+        while (true)
+        {
+            input = Console.ReadLine();
+            if (input == null) break;
+            S.Append(input);
+        }
+        var num = S.ToString().Split(',');
+        foreach (string s in num)
+            res += int.Parse(s);
+        Console.WriteLine(res);
+    }
+}
---
+++ 15663 N과 M (9)/15663 N과 M (9)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Sequence
+{
+    
+}
+class Program
+{
+    static int N, M;
+    static List<int> sequence = new List<int>();
+    static void Main()
+    {
+        var nm = Console.ReadLine().Split();
+        N = int.Parse(nm[0]); M = int.Parse(nm[1]);
+        var numbers = Console.ReadLine().Split();
+    }
+}
