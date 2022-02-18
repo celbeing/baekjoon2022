1e351af038c3b260dc578d84d6d6d207d371a2c8
celbeing <kimsd1983@naver.com>
2022-02-18 18:15:41 +0900

    22.02.18
    이 코드는 박치기 가능성 탐색 코드의 비효율성으로 시간초과가 난 쓰레기 코드입니다.

---
+++ 2580 스토쿠/2580 스토쿠/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+public class Location
+{
+    private int x, y;
+    public int X
+    {
+        get { return this.x; }
+        set { this.x = value; }
+    }
+    public int Y
+    {
+        get { return this.y; }
+        set { this.y = value; }
+    }
+    public Location(int a, int b)
+    {
+        X = a; Y = b;
+    }
+}
+class Program
+{
+    static int[,] sudoku = new int[9, 9];
+    static bool[,] row = new bool[9, 9];
+    static bool[,] column = new bool[9, 9];
+    static bool[,] sector = new bool[9, 9];
+    static Stack<Location> checklist = new Stack<Location>();
+    static Stack<Location> checkedlist = new Stack<Location>();
+    static void Main()
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            var row = Console.ReadLine().Split();
+            for(int j = 0; j < 9; j++)
+            {
+                sudoku[i, j] = int.Parse(row[j]);
+                if (sudoku[i, j] == 0) checklist.Push(new Location(i, j));
+            }
+        }
+
+        Tracking();
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+                Console.Write(sudoku[i, j] + " ");
+            if(i != 8) Console.Write("\n");
+        }
+    }
+
+    static void Tracking()
+    {
+        if (checklist.Count == 0) return;
+        int a = checklist.Peek().X; int b = checklist.Peek().Y;
+        for (int i = 1; i < 10; i++)
+        {
+            sudoku[a, b] = i;
+            if (Check(checklist.Peek()))
+            {
+                checkedlist.Push(checklist.Pop());
+                Tracking();
+            }
+            else continue;
+            if (checklist.Count == 0) return;
+        }
+        sudoku[a, b] = 0;
+        checklist.Push(checkedlist.Pop());
+    }
+
+    static bool Check(Location s)
+    {
+        int a = s.X; int b = s.Y;
+        for(int i = 0; i < 9; i++)
+        {
+            if (i != a && sudoku[i, b] == sudoku[a, b]) return false;
+            if (i != b && sudoku[a, i] == sudoku[a, b]) return false;
+        }
+        a /= 3; b /= 3;
+        a *= 3; b *= 3;
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                if (s.X == a + i && s.Y == b + j) continue;
+                if (sudoku[s.X, s.Y] == sudoku[a + i, b + j]) return false;
+            }
+        }
+        return true;
+    }
+}
