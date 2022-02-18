3c8503500877d37cdf9a5e282a86960c893ee4bd
celbeing <kimsd1983@naver.com>
2022-02-18 20:15:28 +0900

    22.02.18
    새로운 과제입니다. 어서 공부하세요. 제출 기한 일요일.

---
+++ 24479 알고리즘 수업 - 깊이 우선 탐색 1/24479 알고리즘 수업 - 깊이 우선 탐색 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static int N, M, R;
+    static bool[] check;
+    static void Main()
+    {
+        var nmr = Console.ReadLine().Split();
+        N = int.Parse(nmr[0]); M = int.Parse(nmr[1]); R = int.Parse(nmr[2]);
+        check = new bool[N];
+        for(int i = 0; i < M; i++)
+        {
+            var edge = Console.ReadLine().Split();
+            // 그래프를 어떻게 저장하지?
+            // 검색은? 행렬로 하면 320GB짜리
+            // 작동하지 않는 모던워페어 1.5개
+        }
+    }
+    static void DFS(int vertex)
+    {
+        check[vertex] = true;
+    }
+}
