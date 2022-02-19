fa7e92bc14d9a77c1d9c0c65f584b948685435ec
celbeing <kimsd1983@naver.com>
2022-02-19 18:14:35 +0900

    22.02.19
    이런걸 왜 틀렸지..? 1년 전의 나는...

---
+++ 2851 슈퍼 마리오/2851 슈퍼 마리오/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static int near = 0;
+    static int[] mushroom = new int[10];
+    static void Main()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            mushroom[i] = int.Parse(Console.ReadLine());
+            if (i > 0) mushroom[i] += mushroom[i - 1];
+            if (Math.Pow(100 - mushroom[i], 2) <= Math.Pow(100 - near, 2))
+                near = Math.Max(near, mushroom[i]);
+        }
+        Console.WriteLine(near);
+    }
+}
