8759af15089189b24f46e7280668c09f895a4f55
celbeing <kimsd1983@naver.com>
2022-02-19 18:30:00 +0900

    22.02.19
    와... 성장했구나..! 거침없이 정답... 그런데 실버 2...

---
+++ 20162 간식 파티/20162 간식 파티/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static int N, max;
+    static int[] snack, dp;
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        snack = new int[N]; dp = new int[N]; max = 0;
+        for (int i = 0; i < N; i++)
+            snack[i] = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            dp[i] = snack[i];
+            for(int j = 0; j < i; j++)
+            {
+                if (snack[i] > snack[j] && dp[i] < dp[j] + snack[i]) dp[i] = dp[j] + snack[i];
+            }
+            max = dp[i] > max ? dp[i] : max;
+        }
+        Console.WriteLine(max);
+    }
+}
