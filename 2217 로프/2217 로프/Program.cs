9b2068220d612f7a78a586a9913f0c35a44388df
celbeing <kimsd1983@naver.com>
2022-02-19 18:18:42 +0900

    22.02.19
    뭐지.. 왜 틀렸지...

---
+++ 2217 로프/2217 로프/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static int N, max;
+    static int[] rope;
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        rope = new int[N]; max = 0;
+        for (int i = 0; i < N; i++)
+            rope[i] = int.Parse(Console.ReadLine());
+        Array.Sort(rope);
+        for(int i = 0; i < N; i++)
+        {
+            rope[i] *= N - i;
+            max = rope[i] > max ? rope[i] : max;
+        }
+        Console.WriteLine(max);
+    }
+}
