3a01601a8641ec9bfbfd8247fdfe6371067f94fe
celbeing <kimsd1983@naver.com>
2022-02-21 22:53:55 +0900

    22.02.21

---
+++ 1987 알파벳/1987 알파벳/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+class Program
+{
+    static int R, C, x, y, max, index;
+    static int[] dx = { 1, -1, 0, 0 };
+    static int[] dy = { 0, 0, 1, -1 };
+    static char[,] map;
+    static char[] path;
+    static void Main()
+    {
+        var rc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        R = rc[0]; C = rc[1]; index = 0;
+        map = new char[R, C];
+        path = new char[R * C];
+        for(int i = 0; i < R; i++)
+        {
+            var row = Console.ReadLine();
+            for(int j = 0; j < C; j++)
+                map[i, j] = row[j];
+        }
+        dfs(0, 0);
+        Console.WriteLine(max);
+    }
+    static void dfs(int a, int b)
+    {
+        path[index] = (map[a, b]); index++;
+        Console.WriteLine(path);
+        max = max > index ? max : index;
+        for (int i = 0; i < 4; i++)
+        {
+            x = a + dx[i]; y = b + dy[i];
+            if (x < 0 || y < 0 || x > R - 1 || y > C - 1) continue;
+            if (path.Contains(map[x, y])) continue;
+            else dfs(x, y);
+        }
+        path[index] = '0';
+        index--;
+    }
+}
---
+++ 2108 통계학/2108 통계학/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static int N, num, sum, ave, max, min;
+    static int[] count = new int[8001];
+    static int[] maxQ, minQ;
+    static StreamReader sr = new StreamBuffer();
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        maxQ = new int[N]; minQ = new int[N];
+        for(int i = 0; i < N; i++)
+        {
+            num = 
+        }
+    }
+}
