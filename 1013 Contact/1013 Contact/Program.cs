53d409891da798a5f4ebfe1887f69efdede1e90a
celbeing <kimsd1983@naver.com>
2022-02-23 19:14:23 +0900

    22.02.23
    정규표현식을(를) 배웠다!

---
+++ 1013 Contact/1013 Contact/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+class Program
+{
+    static int T;
+    static Regex regex = new Regex("^(100+1+|01)+$");
+    static void Main()
+    {
+        T = int.Parse(Console.ReadLine());
+        for(int i = 0; i < T; i++)
+        {
+            Console.WriteLine(regex.IsMatch(Console.ReadLine()) ? "YES" : "NO");
+        }
+    }
+}
