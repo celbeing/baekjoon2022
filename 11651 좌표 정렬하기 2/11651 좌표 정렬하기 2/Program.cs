0adb73817dba3db4dba9cb716a9f1fb832beba57
celbeing <kimsd1983@naver.com>
2022-02-23 18:00:03 +0900

    22.02.23
    List<(int,int)>를 이용한 방법. 시간초과. 아마도 출력 형식 맞추려고 발광하던 것 때문인듯

---
+++ 11651 좌표 정렬하기 2/11651 좌표 정렬하기 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+class Pragram
+{
+    static int N, x, y;
+    static List<(int, int)> coordinate = new List<(int, int)>();
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        var sb = new StringBuilder();
+        var res = new StringBuilder();
+        for (int i = 0; i < N; i++)
+        {
+            var xy = Console.ReadLine().Split();
+            x = int.Parse(xy[0]); y = int.Parse(xy[1]);
+            coordinate.Add((y,x));
+            coordinate.Sort();
+        }
+        for(int i = 0; i < N; i++)
+        {
+            sb.Append(coordinate[i]);
+            sb.Replace("(",""); sb.Replace(")", ""); sb.Replace(",", "");
+            var xy = sb.ToString().Split();
+            res.AppendLine(xy[1] + " " + xy[0]);
+            sb.Clear();
+        }
+        Console.WriteLine(res);
+    }
+}
