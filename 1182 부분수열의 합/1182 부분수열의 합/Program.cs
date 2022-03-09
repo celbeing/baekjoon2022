1f4ff89fd384c2a0fe5150dc1dba88962c5a91a5
celbeing <kimsd1983@naver.com>
2022-03-09 21:15:41 +0900

    22.03.09

---
+++ 1182 부분수열의 합/1182 부분수열의 합/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static int N, S, sum, count;
+    static int[] arr = new int[20];
+    static void Main()
+    {
+        var ns = Console.ReadLine().Split();
+        N = int.Parse(ns[0]); S = int.Parse(ns[1]);
+        var n = Console.ReadLine().Split();
+        for (int i = 0; i < N; i++)
+            arr[i] = int.Parse(n[i]);
+        for(int i = 0; i < N; i++) Tracking(i);
+        Console.WriteLine(count);
+    }
+    static void Tracking(int index)
+    {
+        sum += arr[index];
+        if (S == sum) count++;
+        for (int i = index + 1; i < N; i++)
+            Tracking(i);
+        sum -= arr[index];
+    }
+}
---
+++ 1931 회의실 배정/1931 회의실 배정/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static int N;
+    static List<List<int>> time1 = new List<List<int>>();
+    static List<List<int>> time2 = new List<List<int>>();
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            var se = Console.ReadLine().Split();
+            time1.Add(new List<int> { int.Parse(se[0]), int.Parse(se[1]) });
+        }
+        var tmp = time1.OrderBy(x => x[1]).OrderBy(x=>x[0]);
+        foreach (var time in tmp)
+            time2.Add(time);
+
+        for (int i = 0; i < N; i++)
+            Console.WriteLine(time2[i][0] + " " + time2[i][1]);
+    }
+}
