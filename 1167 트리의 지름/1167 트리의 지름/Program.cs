3da0e7a1ff22d59428085094a64bb68d5de11670
celbeing <kimsd1983@naver.com>
2022-03-12 17:24:31 +0900

    22.03.12
    트리의 지름은 아이디어를 좀 더 구체화 해보는 것으로...

---
+++ 1167 트리의 지름/1167 트리의 지름/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static int N;
+    static List<List<List<int>>> tree = new List<List<List<int>>>();
+    // tree<vertex<node,distance>>>
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        for(int i = 0; i < N; i++)
+        {
+            var node = Console.ReadLine().Split();
+            for(int j = 1; node[j] != "-1"; j += 2)
+            {
+                tree.Add(new List<List<int>> { });
+                tree[i].Add(new List<int> { int.Parse(node[j]), int.Parse(node[j + 1])});
+            }
+        }
+    }
+}
--- 1931 회의실 배정/1931 회의실 배정/Program.cs
+++ 1931 회의실 배정/1931 회의실 배정/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 class Program
 {
-    static int N;
+    static int N, end, count;
     static List<List<int>> time1 = new List<List<int>>();
     static List<List<int>> time2 = new List<List<int>>();
     static void Main()
@@ -15,11 +15,17 @@ class Program
             var se = Console.ReadLine().Split();
             time1.Add(new List<int> { int.Parse(se[0]), int.Parse(se[1]) });
         }
-        var tmp = time1.OrderBy(x => x[1]).OrderBy(x=>x[0]);
+        var tmp = time1.OrderBy(x => x[0]).OrderBy(x=>x[1]);
         foreach (var time in tmp)
             time2.Add(time);
-
-        for (int i = 0; i < N; i++)
-            Console.WriteLine(time2[i][0] + " " + time2[i][1]);
+        for(int i = 0; i < N; i++)
+        {
+            if(time2[i][0] >= end)
+            {
+                count++;
+                end = time2[i][1];
+            }
+        }
+        Console.WriteLine(count);
     }
 }
