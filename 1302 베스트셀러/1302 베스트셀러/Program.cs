4c76a28e2f796a8f1e23b241fb13d0ed993bf514
celbeing <kimsd1983@naver.com>
2022-03-27 16:47:58 +0900

    22.03.27
    그냥 구조체 안쓰고 했다.

--- 1302 베스트셀러/1302 베스트셀러/Program.cs
+++ 1302 베스트셀러/1302 베스트셀러/Program.cs
@@ -2,55 +2,31 @@
 using System.Collections.Generic;
 class Program
 {
-    static int N, top;
-    static List<Book> sales = new List<Book>();
-    static List<string> bestseller = new List<string>();
+    static int N, max, count, bestseller;
+    static List<string> sales = new List<string>();
     static void Main()
     {
         N = int.Parse(Console.ReadLine());
-        for(int i = 0; i < N; i++)
+        count = 1; bestseller = 0;
+        for (int i = 0; i < N; i++)
+            sales.Add(Console.ReadLine());
+        sales.Sort();
+        for (int i = 0; i < N - 1; i++)
         {
-            var title = Console.ReadLine();
-            var newbook = true;
-            for(int j = 0; j < sales.Count; j++)
+            if (sales[i] == sales[i + 1])
             {
-                if(sales[j].title == title)
+                count++;
+                if (count > max)
                 {
-                    sales[j].Sold();
-                    newbook = false;
-                    break;
+                    bestseller = i;
+                    max = count;
                 }
             }
-            if (newbook)
-                sales.Add(new Book(title));
-        }
-        foreach (var book in sales)
-        {
-            if (top < book.sold)
+            else
             {
-                bestseller.RemoveRange(0, bestseller.Count);
-                bestseller.Add(book.title);
-            }
-            else if(top == book.sold)
-            {
-                bestseller.Add(book.title);
+                count = 1;
             }
         }
-        bestseller.Sort();
-        Console.WriteLine(bestseller[0]);
-    }
-}
-public struct Book
-{
-    public string title;
-    public int sold;
-    public Book(string t)
-    {
-        this.title = t;
-        this.sold = 1;
-    }
-    public void Sold()
-    {
-        this.sold++;
+        Console.WriteLine(sales[bestseller]);
     }
 }
