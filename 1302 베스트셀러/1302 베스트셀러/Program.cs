e093b5b05f755dea88ecce847f3986872314a7bf
celbeing <kimsd1983@naver.com>
2022-03-27 16:34:54 +0900

    22.03.27
    구조체로 팔린 책 목록을 관리하려 했는데 구조체 내부에 선언된 Sold 메소드로 판매 부수가 늘어나지 않는다. 메소드로 넘어가서는 증가하는 게 확인되는데 다시 Main 메소드로 돌아오면 그대로다. 구조체 자체의 값을 직접 외부에서 변경하려고 해도 안된다. 왜지?

---
+++ 1302 베스트셀러/1302 베스트셀러/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static int N, top;
+    static List<Book> sales = new List<Book>();
+    static List<string> bestseller = new List<string>();
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        for(int i = 0; i < N; i++)
+        {
+            var title = Console.ReadLine();
+            var newbook = true;
+            for(int j = 0; j < sales.Count; j++)
+            {
+                if(sales[j].title == title)
+                {
+                    sales[j].Sold();
+                    newbook = false;
+                    break;
+                }
+            }
+            if (newbook)
+                sales.Add(new Book(title));
+        }
+        foreach (var book in sales)
+        {
+            if (top < book.sold)
+            {
+                bestseller.RemoveRange(0, bestseller.Count);
+                bestseller.Add(book.title);
+            }
+            else if(top == book.sold)
+            {
+                bestseller.Add(book.title);
+            }
+        }
+        bestseller.Sort();
+        Console.WriteLine(bestseller[0]);
+    }
+}
+public struct Book
+{
+    public string title;
+    public int sold;
+    public Book(string t)
+    {
+        this.title = t;
+        this.sold = 1;
+    }
+    public void Sold()
+    {
+        this.sold++;
+    }
+}
