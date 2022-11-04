94522e2db337fe91c8aff2eae28e8c7928f25489
celbeing <kimsd1983@naver.com>
2022-11-04 12:23:33 +0900

    22.11.04.

---
+++ 13460 구슬 탈출 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static int N, M;
+    static int[,] board = new int[10, 10];
+    static Queue<int[,]> bfs= new Queue<int[,]>();
+    static void Main()
+    {
+        string a = Console.ReadLine();
+        N = int.Parse(a.Split()[0]);
+        M = int.Parse(a.Split()[1]);
+        for(int i = 0; i < N; i++)
+        {
+            a = Console.ReadLine();
+            for(int j = 0; j < M; j++)
+            {
+                if (a[j] == '#')
+                    board[i, j] = 1;
+                else if (a[j] == '.')
+                    board[i, j] = 0;
+                else if (a[j] == 'O')
+                    board[i, j] = 2;
+                else if (a[j] == 'R')
+                    board[i, j] = 3;
+                else if (a[j] == 'B')
+                    board[i, j] = 4;
+            }
+        }
+    }
+    static int[,] Push(int[,] board, int dir, int depth)
+    {
+        if (dir == 0)
+        {
+
+        }
+        return board;
+    }
+}
