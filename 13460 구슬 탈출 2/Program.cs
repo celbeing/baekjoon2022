ad2f8c2a653a8c06e652ad1461bb10fe3d2fa166
celbeing <kimsd1983@naver.com>
2022-11-23 20:54:58 +0900

    22.11.23.

--- 13460 구슬 탈출 2/Program.cs
+++ 13460 구슬 탈출 2/Program.cs
@@ -21,216 +21,121 @@ public class Location
 }
 class Program
 {
-    static bool red, blue;
     static int N, M;
     static int[,] board = new int[10, 10];
     static Queue<int[,]> bfs= new Queue<int[,]>();
+    static Location hole = new Location(0, 0);
     static void Main()
     {
         string a = Console.ReadLine();
         N = int.Parse(a.Split()[0]);
         M = int.Parse(a.Split()[1]);
+        Location red = new Location(0, 0);
+        Location blue = new Location(0, 0);
         for(int i = 0; i < N; i++)
         {
             a = Console.ReadLine();
             for(int j = 0; j < M; j++)
             {
-                if (a[j] == '#')
+                if (a[j] == '#')        // wall
                     board[i, j] = 1;
-                else if (a[j] == '.')
+                else if (a[j] == '.')   // blank
                     board[i, j] = 0;
-                else if (a[j] == 'O')
-                    board[i, j] = 2;
-                else if (a[j] == 'R')
-                    board[i, j] = 3;
-                else if (a[j] == 'B')
-                    board[i, j] = 4;
+                else if (a[j] == 'O')   // holl
+                {
+                    hole.X = i; hole.Y = j;
+                    board[i, j] = 0;
+                }
+                else if (a[j] == 'R')   // red
+                {
+                    red.X = i; red.Y = j;
+                    board[i, j] = 0;
+                }
+                else if (a[j] == 'B')   // blue
+                {
+                    blue.X = i; blue.Y = j;
+                    board[i, j] = 0;
+                }
             }
         }
     }
-    static int[,] Push(int[,] board, int dir, int depth)
+    static int[,] Push(int dir, Location red, Location blue, int depth)
     {
-        Location goal;
-        Location red = new Location(0, 0);
-        Location blue = new Location(0, 0);
-        for(int i = 0; i < N; i++)
-        {
-            for(int j = 0; j < M; j++)
-            {
-                if (board[i, j] == 2)
-                    goal = new Location(i, j);
-                else if (board[i, j] == 3)
-                    red = new Location(i, j);
-                else if (board[i, j] == 4)
-                    blue = new Location(i, j);
-            }
-        }
-
-        // 1:up, 2:down, 3:left, 4:right
+        // 0:up, 1:down, 2:left, 3:right
         if (dir == 0)
         {
-            if(red.X > blue.X)
+            while (board[red.X - 1, red.Y] == 0 && red != hole)
             {
-                board[blue.X, blue.Y] = 0;
-                for(int i = 0; blue.X - i >= 0; i++)
-                {
-                    if (board[blue.X - i, blue.Y] == 0) continue;
-                    else if (board[blue.X - i, blue.Y] == 2) board[blue.X, blue.Y] = 0;
-                    else
-                    {
-                        board[blue.X, blue.Y] = 0;
-                        board[blue.X - i + 1, blue.Y] = 4;
-                    }
-                }
-                board[red.X, red.Y] = 0;
-                for (int i = 0; red.X - i >= 0; i++)
-                {
-                    if (board[red.X - i, red.Y] == 0) continue;
-                    else if (board[red.X - i, red.Y] == 2) board[red.X, red.Y] = 0;
-                    else
-                    {
-                        board[red.X, red.Y] = 0;
-                        board[red.X - i + 1, red.Y] = 3;
-                    }
-                }
+                if (red.X - 1 == blue.X && red.Y == blue.Y) break;
+                red.X--;
             }
-            else
+            while (board[blue.X - 1, blue.Y] == 0 && blue != hole)
             {
-                board[red.X, red.Y] = 0;
-                for (int i = 0; red.X - i >= 0; i++)
-                {
-                    if (board[red.X - i, red.Y] == 0) continue;
-                    else if (board[red.X - i, red.Y] == 2) board[red.X, red.Y] = 0;
-                    else
-                    {
-                        board[red.X, red.Y] = 0;
-                        board[red.X - i + 1, red.Y] = 3;
-                    }
-                }
-                board[blue.X, blue.Y] = 0;
-                for (int i = 0; blue.X - i >= 0; i++)
-                {
-                    if (board[blue.X - i, blue.Y] == 0) continue;
-                    else if (board[blue.X - i, blue.Y] == 2) board[blue.X, blue.Y] = 0;
-                    else
-                    {
-                        board[blue.X, blue.Y] = 0;
-                        board[blue.X - i + 1, blue.Y] = 4;
-                    }
-                }
+                if (red.X == blue.X - 1 && red.Y == blue.Y) break;
+                blue.X--;
+            }
+            while (board[red.X - 1, red.Y] == 0 && red != hole)
+            {
+                if (red.X - 1 == blue.X && red.Y == blue.Y) break;
+                red.X--;
             }
             return board;
         }
-        else if(dir == 1)
+        else if (dir == 1)
         {
-            if (red.X < blue.X)
+            while (board[red.X + 1, red.Y] == 0 && red != hole)
             {
-                board[blue.X, blue.Y] = 0;
-                for (int i = 0; blue.X + i < N; i++)
-                {
-                    if (board[blue.X + i, blue.Y] == 0) continue;
-                    else if (board[blue.X + i, blue.Y] == 2) board[blue.X, blue.Y] = 0;
-                    else
-                    {
-                        board[blue.X, blue.Y] = 0;
-                        board[blue.X + i - 1, blue.Y] = 4;
-                    }
-                }
-                board[red.X, red.Y] = 0;
-                for (int i = 0; red.X + i < N; i++)
-                {
-                    if (board[red.X + i, red.Y] == 0) continue;
-                    else if (board[red.X + i, red.Y] == 2) board[red.X, red.Y] = 0;
-                    else
-                    {
-                        board[red.X, red.Y] = 0;
-                        board[red.X + i - 1, red.Y] = 3;
-                    }
-                }
+                if (red.X + 1 == blue.X && red.Y == blue.Y) break;
+                red.X++;
             }
-            else
+            while (board[blue.X + 1, blue.Y] == 0 && blue != hole)
             {
-                board[red.X, red.Y] = 0;
-                for (int i = 0; red.X + i < N; i++)
-                {
-                    if (board[red.X + i, red.Y] == 0) continue;
-                    else if (board[red.X + i, red.Y] == 2) board[red.X, red.Y] = 0;
-                    else
-                    {
-                        board[red.X, red.Y] = 0;
-                        board[red.X + i - 1, red.Y] = 3;
-                    }
-                }
-                board[blue.X, blue.Y] = 0;
-                for (int i = 0; blue.X + i < N; i++)
-                {
-                    if (board[blue.X + i, blue.Y] == 0) continue;
-                    else if (board[blue.X + i, blue.Y] == 2) board[blue.X, blue.Y] = 0;
-                    else
-                    {
-                        board[blue.X, blue.Y] = 0;
-                        board[blue.X + i - 1, blue.Y] = 4;
-                    }
-                }
+                if (red.X == blue.X + 1 && red.Y == blue.Y) break;
+                blue.X++;
+            }
+            while (board[red.X + 1, red.Y] == 0 && red != hole)
+            {
+                if (red.X + 1 == blue.X && red.Y == blue.Y) break;
+                red.X++;
             }
             return board;
         }
         else if(dir == 2)
         {
-            if (red.Y > blue.Y)
+            while (board[red.X, red.Y - 1] == 0 && red != hole)
             {
-                board[blue.X, blue.Y] = 0;
-                for (int i = 0; blue.Y - i >= 0; i++)
-                {
-                    if (board[blue.X, blue.Y - i] == 0) continue;
-                    else if (board[blue.X, blue.Y - i] == 2) board[blue.X, blue.Y] = 0;
-                    else
-                    {
-                        board[blue.X, blue.Y] = 0;
-                        board[blue.X - i + 1, blue.Y] = 4;
-                    }
-                }
-                board[red.X, red.Y] = 0;
-                for (int i = 0; red.X - i >= 0; i++)
-                {
-                    if (board[red.X - i, red.Y] == 0) continue;
-                    else if (board[red.X - i, red.Y] == 2) board[red.X, red.Y] = 0;
-                    else
-                    {
-                        board[red.X, red.Y] = 0;
-                        board[red.X - i + 1, red.Y] = 3;
-                    }
-                }
+                if (red.X == blue.X && red.Y - 1 == blue.Y) break;
+                red.Y--;
             }
-            else
+            while (board[blue.X, blue.Y - 1] == 0 && blue != hole)
             {
-                board[red.X, red.Y] = 0;
-                for (int i = 0; red.X - i >= 0; i++)
-                {
-                    if (board[red.X - i, red.Y] == 0) continue;
-                    else if (board[red.X - i, red.Y] == 2) board[red.X, red.Y] = 0;
-                    else
-                    {
-                        board[red.X, red.Y] = 0;
-                        board[red.X - i + 1, red.Y] = 3;
-                    }
-                }
-                board[blue.X, blue.Y] = 0;
-                for (int i = 0; blue.X - i >= 0; i++)
-                {
-                    if (board[blue.X - i, blue.Y] == 0) continue;
-                    else if (board[blue.X - i, blue.Y] == 2) board[blue.X, blue.Y] = 0;
-                    else
-                    {
-                        board[blue.X, blue.Y] = 0;
-                        board[blue.X - i + 1, blue.Y] = 4;
-                    }
-                }
+                if (red.X == blue.X && red.Y == blue.Y - 1) break;
+                blue.Y--;
+            }
+            while (board[red.X, red.Y - 1] == 0 && red != hole)
+            {
+                if (red.X == blue.X && red.Y - 1 == blue.Y) break;
+                red.Y--;
             }
             return board;
         }
         else
         {
+            while (board[red.X, red.Y + 1] == 0 && red != hole)
+            {
+                if (red.X == blue.X && red.Y + 1 == blue.Y) break;
+                red.Y++;
+            }
+            while (board[blue.X, blue.Y + 1] == 0 && blue != hole)
+            {
+                if (red.X == blue.X && red.Y == blue.Y + 1) break;
+                blue.Y++;
+            }
+            while (board[red.X, red.Y + 1] == 0 && red != hole)
+            {
+                if (red.X == blue.X && red.Y + 1 == blue.Y) break;
+                red.Y++;
+            }
             return board;
         }
     }
