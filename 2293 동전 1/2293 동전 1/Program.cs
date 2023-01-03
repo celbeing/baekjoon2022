4eff4eb916752a85cd67a18b1ba5d66d986b69fc
celbeing <kimsd1983@naver.com>
2023-01-03 23:59:56 +0900

    23.01.03.

---
+++ 2293 동전 1/2293 동전 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static int n, k;
+    static string[] input;
+    static uint[] dp = new uint[100001];
+    static void Main()
+    {
+        input = Console.ReadLine().Split();
+        n = int.Parse(input[0]);
+        k = int.Parse(input[1]);
+        dp[0] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            int j = int.Parse(Console.ReadLine());
+            for(int l = j; l <= k; l++)
+            {
+                dp[l] += dp[l - j];
+            }
+
+        }
+        Console.WriteLine(dp[k]);
+    }
+}
