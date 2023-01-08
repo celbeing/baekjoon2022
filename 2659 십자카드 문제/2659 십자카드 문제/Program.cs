d13306225bfa79c8c15b42fa8a01ef1c54b43aef
celbeing <kimsd1983@naver.com>
2023-01-08 23:08:32 +0900

    23.01.08
    이게 97년 초등학생 대상 문제였다는 사실에 현자타임을 갖도록 하자

---
+++ 2659 십자카드 문제/2659 십자카드 문제/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static string[] input;
+    static int[] clock = new int[5];
+    static bool[] check = new bool[10000];
+    static List<int> clockNumbers = new List<int>();
+    static void Main()
+    {
+        input = Console.ReadLine().Split();
+        for(int i = 0; i < 4; i++)
+        {
+            clock[1] *= 10;
+            clock[1] += int.Parse(input[i]);
+        }
+        for(int i = 0; i < 3; i++)
+            clock[2 + i] = (clock[1 + i] % 1000) * 10 + clock[1 + i] / 1000;
+        clock[0] = Math.Min(Math.Min(clock[1], clock[2]), Math.Min(clock[3], clock[4]));
+        for(int i = 1111; i < 10000; i++)
+        {
+            if (check[i]) continue;
+            if ($"{i}".Contains('0')) continue;
+            clockNumbers.Add(i);
+            int cnt = i;
+            for(int j = 0; j < 3; j++)
+            {
+                cnt = (cnt % 1000) * 10 + cnt / 1000;
+                check[cnt] = true;
+            }
+        }
+        Console.WriteLine(clockNumbers.IndexOf(clock[0]) + 1);
+    }
+}
