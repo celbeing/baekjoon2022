46fa00853b31b67d90a7019081c687df71f14a34
celbeing <kimsd1983@naver.com>
2023-01-10 23:35:42 +0900

    23.01.10

---
+++ 2641 다각형그리기/2641 다각형그리기/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+class Program
+{
+    static int L, T, count;
+    static StringBuilder result = new StringBuilder();
+    static void Main()
+    {
+        L = int.Parse(Console.ReadLine());
+        int[] sample = new int[L];
+        string[] input = new string[L];
+        input = Console.ReadLine().Split();
+        for (int i = 0; i < L; i++) sample[i] = int.Parse(input[i]);
+        T = int.Parse(Console.ReadLine());
+        int[,] cases = new int[T, L];
+        for (int i = 0; i < T; i++)
+        {
+            input = Console.ReadLine().Split();
+            for (int j = 0; j < L; j++) cases[i, j] = int.Parse(input[j]);
+            for (int j = 0; j < L; j++)
+            {
+                int k = cases[i, 0];
+                for (int l = 0; l < L - 1; l++)
+                    cases[i, l] = cases[i, l + 1];
+                cases[i, L - 1] = k;
+                if (check(sample, cases, i))
+                {
+                    count++;
+                    for (int l = 0; l < L - 1; l++)
+                        result.Append($"{input[l]} ");
+                    result.Append($"{input[L - 1]}\n");
+                }
+            }
+        }
+        if(count == 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+        result.Remove(result.Length - 1, 1);
+        Console.WriteLine(count);
+        Console.WriteLine(result.ToString());
+    }
+    static bool check(int[] sample, int[,] cases, int t)
+    {
+        for (int i = 0; i < L; i++)
+            if (sample[i] != cases[t, i]) goto reverse;
+        return true;
+    reverse:
+        int[] reverse = new int[L];
+        for(int i = 0; i < L; i++)
+        {
+            switch(cases[t, L - i - 1])
+            {
+                case 1:
+                    reverse[i] = 3;
+                    break;
+                case 2:
+                    reverse[i] = 4;
+                    break;
+                case 3:
+                    reverse[i] = 1;
+                    break;
+                default:
+                    reverse[i] = 2;
+                    break;
+            }
+        }
+        for (int i = 0; i < L; i++)
+            if (sample[i] != reverse[i]) return false;
+        return true;
+    }
+}
---
+++ 2650 교차점개수/2650 교차점개수/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static int dot, sum, max;
+    static string[] strI = new string[4];
+    static int[] intI = new int[4];
+    static void Main()
+    {
+        dot = int.Parse(Console.ReadLine());
+        int[,] dots = new int[dot / 2, 2];
+        int[] meets = new int[dot / 2];
+        for (int i = 0; i < dot / 2; i++)
+        {
+            // 1: top, 2: bottom, 3: left, 4: right
+            strI = Console.ReadLine().Split();
+            for (int j = 0; j < 4; j++) intI[j] = int.Parse(strI[j]);
+            int k;
+            switch (intI[0])
+            {
+                case 1:
+                    k = 0;
+                    break;
+                case 2:
+                    k = -150;
+                    break;
+                case 3:
+                    k = -200;
+                    break;
+                default:
+                    k = 50;
+                    break;
+            }
+            dots[i, 0] = intI[1] + k;
+            switch (intI[2])
+            {
+                case 1:
+                    k = 0;
+                    break;
+                case 2:
+                    k = -150;
+                    break;
+                case 3:
+                    k = -200;
+                    break;
+                default:
+                    k = 50;
+                    break;
+            }
+            dots[i, 1] = intI[3] + k;
+            if (dots[i, 0] < 0) dots[i, 0] *= -1;
+            if (dots[i, 1] < 0) dots[i, 1] *= -1;
+            if (dots[i, 0] > dots[i, 1])
+            {
+                k = dots[i, 0];
+                dots[i, 0] = dots[i, 1];
+                dots[i, 1] = k;
+            }
+        }
+        for(int i = 0; i < dot/2; i++)
+        {
+            for(int j = 0; j < dot/2; j++)
+            {
+                if (i == j) continue;
+                if ((dots[i, 0] < dots[j, 0] && dots[j, 0] < dots[i, 1]) ^ (dots[i, 0] < dots[j, 1] && dots[j, 1] < dots[i, 1])) meets[i]++;
+            }
+            sum += meets[i];
+            if (max < meets[i]) max = meets[i];
+        }
+        Console.WriteLine(sum / 2);
+        Console.WriteLine(max);
+    }
+}
