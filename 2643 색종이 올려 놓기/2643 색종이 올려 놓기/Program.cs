39f03bd95e2637b52afb49b8926b8cb0ccf2ecc9
celbeing <kimsd1983@naver.com>
2023-01-12 14:09:57 +0900

    23.01.12

---
+++ 2643 색종이 올려 놓기/2643 색종이 올려 놓기/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static int N;
+    static string[] input = new string[2];
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        int[,] paper = new int[N, 2];
+        for(int i = 0; i < N; i++)
+        {
+            input = Console.ReadLine().Split();
+            paper[i, 0] = int.Parse(input[0]);
+            paper[i, 1] = int.Parse(input[1]);
+        }
+    }
+}
