916e8c360a4cd1480b42c4a1a7985a68292e6c23
celbeing <kimsd1983@naver.com>
2023-01-15 03:16:27 +0900

    23.01.15.
    왜 잠이 안 오는 것인지...

--- 2643 색종이 올려 놓기/2643 색종이 올려 놓기/Program.cs
+++ 2643 색종이 올려 놓기/2643 색종이 올려 놓기/Program.cs
@@ -1,18 +1,39 @@
 ﻿using System;
+using System.Linq;
 
 class Program
 {
-    static int N;
-    static string[] input = new string[2];
+    static int N, count;
     static void Main()
     {
         N = int.Parse(Console.ReadLine());
-        int[,] paper = new int[N, 2];
-        for(int i = 0; i < N; i++)
+        int[] sort = new int[N];
+        int[,] paper = new int[N, 3];
+        for (int i = 0; i < N; i++)
         {
-            input = Console.ReadLine().Split();
-            paper[i, 0] = int.Parse(input[0]);
-            paper[i, 1] = int.Parse(input[1]);
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            sort[i] = Math.Min(input[0], input[1]) * 1000 + Math.Max(input[0], input[1]);
         }
+        Array.Sort(sort);
+        for (int i = N - 1; i >= 0; i--)
+        {
+            paper[i, 0] = sort[N - i - 1] / 1000;
+            paper[i, 1] = sort[N - i - 1] % 1000;
+        }
+        paper[0, 2] = 1;
+        count = 1;
+        for (int i = 1; i < N; i++)
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (paper[i, 0] <= paper[j, 0] && paper[i, 1] <= paper[j, 1])
+                {
+                    paper[i, 2] = Math.Max(paper[j, 2] + 1, paper[i, 2]);
+                }
+            }
+            if (paper[i, 2] == 0) paper[i, 2]++;
+            count = Math.Max(paper[i, 2], count);
+        }
+        Console.WriteLine(count);
     }
 }
