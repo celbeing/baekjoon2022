683a3fec35e834e1ae00cb0d765e1c50da3fe90e
celbeing <kimsd1983@naver.com>
2023-01-17 00:06:53 +0900

    23.01.17.

---
+++ 2636 치즈/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static int width, height, time, last;
+    static int[] dr = { 1, 0, -1, 0 };
+    static int[] dc = { 0, 1, 0, -1 };
+    static Queue<int> exposed = new Queue<int>();
+
+}
