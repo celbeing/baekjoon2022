6fc94397b851afaede9d21906ab750b7e5a81364
celbeing <kimsd1983@naver.com>
2023-01-16 22:05:31 +0900

    23.01.16.

---
+++ 2635 수 이어가기/2635 수 이어가기/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static int N;
+    static int a, b, c;
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        a = 1; b = 1;
+        
+    }
+}
