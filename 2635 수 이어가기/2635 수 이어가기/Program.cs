1cc5b34083abbaace3e686342c64890aa3b66d69
celbeing <kimsd1983@naver.com>
2023-01-16 22:56:34 +0900

    23.01.16.
    오랜만에 진짜 쉬운 영양가 없는 문제였습니다.

--- 2635 수 이어가기/2635 수 이어가기/Program.cs
+++ 2635 수 이어가기/2635 수 이어가기/Program.cs
@@ -2,12 +2,36 @@
 
 class Program
 {
-    static int N;
-    static int a, b, c;
+    static int N, max, index, a1, a2, a3;
     static void Main()
     {
         N = int.Parse(Console.ReadLine());
-        a = 1; b = 1;
-        
+        int[] count = new int[N];
+        max = 0;
+        for (int i = 0; i < N; i++)
+        {
+            a1 = N; a2 = i + 1; a3 = 0; count[i] = 2;
+            while (true)
+            {
+                a3 = a1 - a2;
+                if (a3 < 0) break;
+                a1 = a2; a2 = a3; count[i]++;
+            }
+            if (count[i] > max)
+            {
+                max = count[i];
+                index = i;
+            }
+        }
+        Console.WriteLine(max);
+        Console.Write($"{N} {index + 1}");
+        a1 = N; a2 = index + 1;
+        while (true)
+        {
+            a3 = a1 - a2;
+            if (a3 < 0) break;
+            Console.Write($" {a3}");
+            a1 = a2; a2 = a3;
+        }
     }
 }
