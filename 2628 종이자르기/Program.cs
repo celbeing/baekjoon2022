53a2d080f6ab2b13c6dc910c68f62ff2551d0da8
celbeing <kimsd1983@naver.com>
2023-01-18 02:10:35 +0900

    23.01.17.

---
+++ 2628 종이자르기/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static int width, height, n;
+    static int[] w, h;
+    static Stack<int> ws = new Stack<int>();
+    static Stack<int> hs = new Stack<int>();
+    static void Main()
+    {
+        int[] wh = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        width = wh[0]; height = wh[1];
+        n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            wh = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            if (wh[0] == 0) hs.Push(wh[1]);
+            else ws.Push(wh[1]);
+        }
+        w = new int[ws.Count + 2];
+        h = new int[hs.Count + 2];
+        for (int i = 1; ws.Count > 0; i++) w[i] = ws.Pop();
+        for (int i = 1; hs.Count > 0; i++) h[i] = hs.Pop();
+        w[0] = 0; h[0] = 0;
+        w[w.Length - 1] = width;
+        h[h.Length - 1] = height;
+        Array.Sort(w);
+        Array.Sort(h);
+        width = 0; height = 0;
+        for (int i = 1; i < w.Length; i++) width = Math.Max(width, w[i] - w[i - 1]);
+        for (int i = 1; i < h.Length; i++) height = Math.Max(height, h[i] - h[i - 1]);
+        Console.WriteLine(width * height);
+    }
+}
--- 2636 치즈/Program.cs
+++ 2636 치즈/Program.cs
@@ -4,9 +4,74 @@ using System.Collections.Generic;
 
 class Program
 {
-    static int width, height, time, last;
+    static int row, column, time, last;
     static int[] dr = { 1, 0, -1, 0 };
     static int[] dc = { 0, 1, 0, -1 };
+    static bool[,] cheese, check;
     static Queue<int> exposed = new Queue<int>();
-
+    static Queue<int> air = new Queue<int>();
+    static void Main()
+    {
+        int[] rc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        row = rc[0]; column = rc[1];
+        cheese = new bool[row, column];
+        check = new bool[row, column];
+        for (int i = 0; i < row; i++)
+        {
+            int[] r = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int j = 0; j < column; j++)
+            {
+                if (r[j] == 1)
+                {
+                    cheese[i, j] = true;
+                    last++;
+                }
+                else cheese[i, j] = false;
+            }
+        }
+        air.Enqueue(0);
+        Melt();
+    }
+    static void Melt()
+    {
+        while (true)
+        {
+            while (air.Count > 0)
+            {
+                int r = air.Peek() / 100;
+                int c = air.Dequeue() % 100;
+                for (int i = 0; i < 4; i++)
+                {
+                    int rr = r + dr[i];
+                    int cc = c + dc[i];
+                    if (rr < 0 || rr >= row || cc < 0 || cc >= column || check[rr, cc]) continue;
+                    if (cheese[rr, cc])
+                    {
+                        check[rr, cc] = true;
+                        exposed.Enqueue(rr * 100 + cc);
+                    }
+                    else
+                    {
+                        check[rr, cc] = true;
+                        air.Enqueue(rr * 100 + cc);
+                    }
+                }
+            }
+            if (exposed.Count == last)
+            {
+                if (last != 0) Console.WriteLine(time + 1);
+                else Console.WriteLine(time);
+                Console.WriteLine(last);
+                return;
+            }
+            while (exposed.Count > 0)
+            {
+                int r = exposed.Peek() / 100;
+                int c = exposed.Peek() % 100;
+                cheese[r, c] = false; last--;
+                air.Enqueue(exposed.Dequeue());
+            }
+            time++;
+        }
+    }
 }
