aa22368dffb2a17f427263b0cd026d81a8bf05a4
celbeing <kimsd1983@naver.com>
2023-01-18 18:33:09 +0900

    23.01.18.

---
+++ 2670 연속부분최대곱/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static int N;
+    static double max;
+    static double[] dp;
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        dp = new double[N];
+        dp[0] = double.Parse(Console.ReadLine());
+        max = dp[0];
+        for(int i = 1; i < N; i++)
+        {
+            double z = double.Parse(Console.ReadLine());
+            dp[i] = Math.Max(dp[i - 1] * z, z);
+            max = Math.Max(max, dp[i]);
+        }
+        Console.WriteLine(max.ToString("0.000"));
+    }
+}
