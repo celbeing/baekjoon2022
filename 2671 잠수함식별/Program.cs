300bca423037468a867d5084b268e70093c341b0
celbeing <kimsd1983@naver.com>
2023-01-18 19:13:02 +0900

    23.01.18.
    정규표현식

---
+++ 2671 잠수함식별/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    
+    static void Main()
+    {
+        string pattern = "^(100+1+|01)+$";
+        if (Regex.IsMatch(Console.ReadLine(), pattern)) Console.WriteLine("SUBMARINE");
+        else Console.WriteLine("NOISE");
+    }
+}
