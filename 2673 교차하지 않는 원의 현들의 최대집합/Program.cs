92a76c3a07efa7d16e9d55e9f53bd5a87e1e1c85
celbeing <kimsd1983@naver.com>
2023-01-21 17:14:55 +0900

    23.01.21.

---
+++ 2673 교차하지 않는 원의 현들의 최대집합/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static int N;
+    static int[,] dp = new int[100, 100];
+    static int[,] str = new int[100, 100];
+    static void Main()
+    {
+        N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            int[] ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            str[ab[0] - 1, ab[1] - 1] = 1; str[ab[1] - 1, ab[0] - 1] = 1;
+        }
+        for (int i = 0; i < 100; i++)
+            for (int j = i; j >= 0; j--)
+                for (int k = j; k < i; k++)
+                    dp[j, i] = Math.Max(dp[j, i], dp[j, k] + dp[k, i] + str[i, j]);
+        Console.WriteLine(dp[0, 99]);
+    }
+}
